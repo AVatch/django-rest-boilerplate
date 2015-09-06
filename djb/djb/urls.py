a345495c9e9d0bd17233a08df51e9e1aaf991c84
AVatch <avatchinsky@outlook.com>
2015-09-06 11:14:57 -0400

    hooked up a REST api overview

--- djb/djb/urls.py
+++ djb/djb/urls.py
@@ -3,10 +3,12 @@ from django.conf.urls import include, url, patterns
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
+import views
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 
+    url(r'^api/' + settings.API_VERSION + '/$', views.api_root),
     url(r'^api/' + settings.API_VERSION + '/', include('accounts.urls')),
 )
 
